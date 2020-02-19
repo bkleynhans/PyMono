d1de16fffde7a14f392ed5d81f617f6297e4dcce
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2020-02-18 20:24:33 -0500

    Just some changes in spacing in the file

--- gui/forms/base_classes/gui_window.py
+++ gui/forms/base_classes/gui_window.py
@@ -19,31 +19,31 @@ from tkinter import ttk
 
 
 class Gui_Window():
-        
+
     # Settings Window constructor
     def __init__(self, master, window_name, window_title):
-                
+
         self.window_name = window_name
         self.window_title = window_title
-        
+
         self.create_gui_window(master)
-        
-    
+
+
     # Create the actual window as a separate window
     def create_gui_window(self, master):
-        
+
         self.gui_window = Toplevel(master)
-        
+
         # Create frame container to easily access frames from other areas in the gui
         self.frames = {}
         self.gui_window.frames = self.frames
-        
+
         # Create notebook container to easily access notebooks from other areas in the gui
         self.notebooks = {}
         self.gui_window_notebooks = self.notebooks
-        
+
         # Create widgets container to easily access widgets from other areas in the gui
         self.widgets = {}
         self.gui_window.widgets = self.widgets
-        
+
         master.windows[self.window_name] = self.gui_window
