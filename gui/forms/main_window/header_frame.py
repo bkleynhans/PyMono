3af745e479647f3778c79babdfced758d8f467de
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2020-02-23 17:17:16 -0500

    Add reference to root object

--- gui/forms/main_window/header_frame.py
+++ gui/forms/main_window/header_frame.py
@@ -1,10 +1,8 @@
 ###
 #
-# CIS Top of Atmosphere Radiance Calibration
 #
-# Program Description : The header frame class contains the conditions for the header of 
-#                       the program.  From here the user selects which algorithm they want
-#                       to calculate (SC - Single Channel; TOA - Top of Atmosphere;SW - Split Window)
+#
+# Program Description :
 # Created By          : Benjamin Kleynhans
 # Creation Date       : May 23, 2019
 # Authors             : Benjamin Kleynhans
@@ -22,16 +20,16 @@ from gui.forms.base_classes.gui_frame import Gui_Frame
 import pdb
 
 class Header_Frame(Gui_Frame):
-    
+
     # Header Frame constructor
-    def __init__(self, master):
-        
-        Gui_Frame.__init__(self, master, "header_frame")
+    def __init__(self, root, master):
+
+        Gui_Frame.__init__(self, root, master, "header_frame")
         self.create_header(master)
-        
-        
+
+
     # Create header frame object
     def create_header(self, master):
-        
+
         master.frames[self.frame_name].pack(padx = 10, pady = (20, 0))
-        
+
