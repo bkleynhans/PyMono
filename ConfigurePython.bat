56e866aeda65b7949f14a8e1a12a176c9741a6aa
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2020-10-31 15:53:20 -0400

    Updated the dependencies for Spyder from Jedi 0.14.1 to 0.17.1

--- ConfigurePython.bat
+++ ConfigurePython.bat
@@ -1,1 +1,1 @@
-python -m pip install -Iv --upgrade pip pyqt5==5.12.3 spyder PyQtWebEngine==5.12.1 numpy scipy matplotlib pandas numpy nose jedi==0.14.1 pythonnet
+python -m pip install -Iv --upgrade pip pyqt5==5.12.3 spyder PyQtWebEngine==5.12.1 numpy scipy matplotlib pandas numpy nose jedi==0.17.1 pythonnet
